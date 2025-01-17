c43b8f5435d763016f4c228c32fea0f12c6529ef
Blanton <26BlantonJ@badinhs.org>
2025-01-17 13:43:05 -0500

    added notes

---
+++ dataTypeNotes.js
@@ -0,0 +1,10 @@
+/* 
+There are 8 data types in javascript
+
+Numbers (both floats and integers)
+    also infinity and NaN
+BigInt
+    for MASSIVE numbers that javascript cant comprehend which is made by using a lowercase "n"
+String
+    Letters, Words, Characters, Symbols treated like text made with quotes
+*/
