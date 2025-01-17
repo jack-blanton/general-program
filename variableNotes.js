becbc58a15ea15cb558e735506b913801ebd4af1
Blanton <26BlantonJ@badinhs.org>
2025-01-17 13:14:11 -0500

    finished notes

--- variableNotes.js
+++ variableNotes.js
@@ -2,5 +2,63 @@
 // We will use a combination of comments and code to build up our notes.
 
 // We start by declaring a variable
-let myName = "jeff";
-myName;
+// let myName = "jeff";
+// myName;
+// console.log(myName);
+
+// // Here is a different way to make a variable
+
+// const e = 2.7;
+// console.log(e);
+// e = 3.1415;
+// console.log(e);
+//error because you cant change a constant!!!
+
+// a third variable!!!
+// var pi = 3.1415;
+// console.log(pi);
+// pi = e;
+// console.log(pi);
+
+//var and let are interchangable constant isnt changeable blah blah yap yap
+
+// example to show how fast computers can calculate numbers!!!!!! miau miau miau miau
+// let total = 0;
+// let i = 0;
+// while (i < 1001){
+//     total = total + i;
+//     i = i + 1;
+// }
+// console.log(total);
+
+console.log(23 + 97)
+
+let sumOfSix = 1 + 2 + 3 + 5 + 6 + 7;
+console.log(sumOfSix);
+
+// you can do math in the console including using variables from the file
+// simulation of calculating percent error
+const actual = 57;
+let prediction = actual - 13;
+let percentage = prediction / actual;
+let percentError = 1 - percentage;
+
+/* starts a multiline or block, comment /*
+/*
+Math operators in javascript
+Addition is +
+Subtraction is -
+Multiplication is *
+Division is /
+Exponent is **
+Modulus is %=
+
+Javascript has order of operations 
+Modulus is with multiplication and division
+
+Javascript only has number, no int and float
+
+Variables can only contain letters, numbers, dollar signs and underscores
+    The first number cant be a digit
+    Cant be reserved words like let or const or console.log type shi
+*/
