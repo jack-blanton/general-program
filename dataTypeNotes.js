38029bd4960477f886882ded11c5fd3d6ba1feb5
Blanton <26BlantonJ@badinhs.org>
2025-01-21 13:48:47 -0500

    added notes on data types

--- dataTypeNotes.js
+++ dataTypeNotes.js
@@ -7,4 +7,25 @@ BigInt
     for MASSIVE numbers that javascript cant comprehend which is made by using a lowercase "n"
 String
     Letters, Words, Characters, Symbols treated like text made with quotes
+    + with string attatches them together
+    could also use 'stringoty' or `stringothan` for things like 'mr. smith's class'
+    backticks `` allow you to embed variables into a string
+        let myName = "Jack"
+        console.log(`My name is ${myName}`);
+    you can also put any operation using this thing
+        console.log(`2 + 3 = ${2 + 3}`);
+Char
+    1 character, javascript doesnt have this
+Boolean
+    True or False
+Null
+    only has null which means nothing, empty or unknown
+undefined
+    undefined, no data assigned
+Objects and symbols exist but they're not relevant or something
+
+typeof fucntion lets you determine what type of info something is
+console.log(typeof("sigma")) -> string
+console.log(typeof(365)) -> number
 */
+  
