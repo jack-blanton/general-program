0c3b08207cf446acd8035bf2f47640cb454d573a
Blanton <26BlantonJ@badinhs.org>
2025-01-23 13:18:56 -0500

    added notes

---
+++ conditionalsNotes.js
@@ -0,0 +1,16 @@
+/* 
+A conditional statement is a "If -> Then" statement
+Often they rely on comparisons
+Comparisons look like math equations in like all programming languages
+A < B (Is a less than b?)
+A > B (Is a greater than b?)
+A == B (Is a equal to b?)
+A <= B (Is a less than or equal to b?)
+A >= B (Is a greater than or equal to b?)
+Javascript can read strings that are singular numbers and compare them with proper numbers
+    (5 == "5") -> true
+However, if you use === it also compares data type
+    (5 === "5") -> false
+If you put an ! in any of these it means NOT
+    (5 != 3) -> true
+*/
