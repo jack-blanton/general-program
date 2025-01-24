fc1c2110c2c520f336154391cd839c0a388abae7
Blanton <26BlantonJ@badinhs.org>
2025-01-24 13:02:31 -0500

    added notes

--- conditionalsNotes.js
+++ conditionalsNotes.js
@@ -13,4 +13,20 @@ However, if you use === it also compares data type
     (5 === "5") -> false
 If you put an ! in any of these it means NOT
     (5 != 3) -> true
+You can compare strings by the first letter in them, if they're equal it goes to the next one
+and then so on and so on
+Greater = later in alphabet and capitals
 */
+if(3 < 5){
+    console.log("rizz");
+}
+// If statements allow you to gate codes behind conditions being true
+// Other things do this but also allow you to accomplish other things like else and elif
+if(7<5){
+    console.log("you wrong buh!");
+} else{
+    console.log("ding ding ding");
+}
+
+let testNumber = 6
+if(testNumber > 5){}
