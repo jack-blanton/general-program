1664eea4974fcb09af87946c812a388a64e43984
Blanton <26BlantonJ@badinhs.org>
2025-02-19 13:41:33 -0500

    added notes on array function things like push and shift

--- loopsNotes.js
+++ loopsNotes.js
@@ -47,31 +47,103 @@
 // }
 // console.log(sum);
 
-let sum = 0;
-let max = +prompt("Add the numbers from 1 to what?");
-let now = Date.now();
-console.log(now);
-for (let i = 1; i <= max; i++){
-    sum += i;
-    if(Date.now() > now + 1000){
-        console.log("you slow as hell!")
-        break;
-    }
-}
-console.log(sum);
+// let sum = 0;
+// let max = +prompt("Add the numbers from 1 to what?");
+// let now = Date.now();
+// console.log(now);
+// for (let i = 1; i <= max; i++){
+//     sum += i;
+//     if(Date.now() > now + 1000){
+//         console.log("you slow as hell!")
+//         break;
+//     }
+// }
+// console.log(sum);
 
 // continue is a lighter version of break
 // instead of breaking out of loop, it skips to the end of the current iteration
 
-for (let i = 0; i < 21; i++){
-    if(i % 2 == 0){
-        continue;
-    }
-    console.log(i);
-}
+// for (let i = 0; i < 21; i++){
+//     if(i % 2 == 0){
+//         continue;
+//     }
+//     console.log(i);
+// }
 // Three kinds of loops
 // For, While, Do While
 
 // Do While loop does it at least once
 // break ejects loop
 // continue skips the rest of the current loop
+
+//In programming, an array is a collection of ordered values
+// let arrayName = [item1, item 2, item3];
+// there are 2 ways to make an empty array
+
+// let fakearray = new Array();
+// let fakearray = [];
+
+// let fruits =["apples", "bananas", "canteloupes", "durians", "lychees"]
+// console.log(fruits);
+// console.log(fruits[3]);
+// fruits[3] = "watermelons";
+// console.log(fruits);
+// fruits[5] = "strawberries"
+// console.log(fruits);
+// console.log(fruits.length)
+// fruits[fruits.length] = "dragonfruit"
+// console.log(fruits);
+// let exampleArray2 = ["apple", 35, true, function () {console.log("hello is anyone in there")}, "array"]
+// console.log(exampleArray2[3])
+
+// // you can do math in arrays
+// let obj2 = [3,5];
+// let obj1 = [7, 11];
+// let dist = Math.sqrt((obj2[0] - obj1[0])**2 + (obj2[1] - obj1[1])**2);
+// console.log(dist)
+let primes = [2,3,5,7,11,13,17];
+// let i = 0;
+// while (i < primes.length){
+//     console.log(primes[i]);
+//     i++;
+// }
+// console.log(primes.at(-1));
+
+// .pop() finds the last element of the array, returns it and then deletes it
+// console.log(primes.pop());
+// console.log(primes)
+
+
+// let sum = 0;
+// while(primes.length > 0) {
+//     sum += primes.pop();
+//     console.log(primes, sum);
+// }
+
+// .push adds an element to the end of the array
+let fruits = ["apple", "banana", "cantaloupe"];
+fruits.push("watermelon");
+console.log(fruits);
+
+// .shift is pop but at the beginning
+
+fruits.shift();
+console.log(fruits);
+
+// .unshift is push but at the beginning
+
+fruits.unshift("apricot");
+console.log(fruits);
+
+fruits.push("orange","peach");
+fruits.unshift("lemon", "pineapple");
+
+console.log(fruits);
+
+for(let i = 0; i < fruits.length; i++){
+    console.log(fruits[i]);
+}
+
+for (let fruit of fruits) {
+    console.log(fruit);
+}
